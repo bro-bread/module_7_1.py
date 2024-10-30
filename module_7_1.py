759617f6b8810c962f0bf8f2ee20500741052cf1
bro-bread <sasaglebov928@gmail.com>
2024-10-31 02:44:48 +0700

    module_7_1.py(2)

--- module_7_1.py
+++ module_7_1.py
@@ -1,5 +1,4 @@
 import os
-
 class Product:
     def __init__(self, name, weight, category):
         self.name = str(name)
@@ -7,21 +6,22 @@ class Product:
         self.category = str(category)
     def __str__(self):
         return f"{self.name}, {self.weight}, {self.category}"
-
 class Shop:
     def __init__(self, filename='products.txt.txt'):
         self.filename = filename
-
     def get_products(self):
-        with open(self.filename, encoding='utf-8') as file:
+        with (open(self.filename, encoding='utf-8') as file):
             content = file.read()
-            print(content)
-
+            return content
     def add(self, *products):
         file2 = open(self.filename, "r+")
         t2x2 = file2.read()
         for er in products:
             er2 = str(er.name)
+            er3 = str(er.weight)
+            er4 = str(er.category)
+            er5 = (er2,er3,er4)
+            er6 = str (er5)
             if er.name in t2x2:
                 print("Продукт ", er2," уже есть в магазине'")
             else:
@@ -30,16 +30,12 @@ class Shop:
                     bate = os.path.getsize('products.txt.txt')
                     file2.seek(bate)
                     file2.write("\n")
-                    file2.write(er2)
+                    file2.write(er6)
                     file2.close()
-
 s1 = Shop()
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
-
 print(p2) # __str__
-
 s1.add(p1, p2, p3)
-
 print(s1.get_products())
